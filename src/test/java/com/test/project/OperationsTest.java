0ddec61db5ed948b18be1fe968204be2d11f68d0
Vladislav <vlad.shaldymov@mail.ru>
2022-12-27 23:24:00 +0500

    refactor code

--- src/test/java/com/test/project/OperationsTest.java
+++ src/test/java/com/test/project/OperationsTest.java
@@ -9,79 +9,80 @@ import static org.junit.Assert.assertEquals;
 public class OperationsTest {
 
     private static final double DELTA = 1e-15;
+
     private final Operations operations = new Operations();
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
 
     @Test
-    public void checkSumReturnCorrectResult() {
+    public void checkSumReturnCorrectResultWithPositiveNumber() {
         int result = operations.sum(5, 4);
         assertEquals(9, result);
     }
 
     @Test
-    public void checkSumReturnCorrectResult1() {
+    public void checkSumReturnCorrectResultWithNegativeNumber() {
         int result = operations.sum(-4, 9);
         assertEquals(5, result);
     }
 
     @Test
-    public void checkSumReturnCorrectResult2() {
+    public void checkSumReturnCorrectResultWithZero() {
         int result = operations.sum(8, 0);
         assertEquals(8, result);
     }
 
     @Test
-    public void checkMinusReturnCorrectResult() {
+    public void checkMinusReturnCorrectResultWithPositiveNumber() {
         int result = operations.minus(192, 156);
         assertEquals(36, result);
     }
 
     @Test
-    public void checkMinusReturnCorrectResult1() {
+    public void checkMinusReturnCorrectResultWithNegativeNumber() {
         int result = operations.minus(-4, 20);
         assertEquals(-24, result);
     }
 
     @Test
-    public void checkMinusReturnCorrectResult2() {
+    public void checkMinusReturnCorrectResultWithZero() {
         int result = operations.minus(-8, 0);
         assertEquals(-8, result);
     }
 
     @Test
-    public void checkMultiplyReturnCorrectResult() {
+    public void checkMultiplyReturnCorrectResultWithPositiveNumber() {
         int result = operations.multiply(10, 2);
         assertEquals(20, result);
     }
 
     @Test
-    public void checkMultiplyReturnCorrectResult1() {
+    public void checkMultiplyReturnCorrectResultWithNegativeNumber() {
         int result = operations.multiply(-20, 3);
         assertEquals(-60, result);
     }
 
     @Test
-    public void checkMultiplyReturnCorrectResult2() {
+    public void checkMultiplyReturnCorrectResultWithZero() {
         int result = operations.multiply(15, 0);
         assertEquals(0, result);
     }
 
     @Test
-    public void checkDivisionReturnCorrectResult() {
+    public void checkDivisionReturnCorrectResultWithPositiveNumber() {
         double result = operations.division(36, 6);
         assertEquals(6, result, DELTA);
     }
 
     @Test
-    public void checkDivisionReturnCorrectResult1() {
+    public void checkDivisionReturnCorrectResult1WithNegativeNumber() {
         double result = operations.division(-81, 9);
         assertEquals(-9, result, DELTA);
     }
 
     @Test()
-    public void checkDivisionReturnCorrectResult2() {
+    public void checkExceptionIsThrownWhenDivideByZeroPerformed() {
         exceptionRule.expect(IllegalArgumentException.class);
         exceptionRule.expectMessage("Деление на ноль запрещено!");
 
